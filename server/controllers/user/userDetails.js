85166c5b91e64ef45f6bdcda0d7092b7dd2813db
yash-helix <yash.netankar@helixstack.in>
2022-11-01 19:58:12 +0530

    testing ip

--- server/controllers/user/userDetails.js
+++ server/controllers/user/userDetails.js
@@ -4,20 +4,22 @@ import axios from 'axios';
 import { Reader } from 'maxmind';
 import fs from 'fs'
 
-export const getCountry = () => {
+export const getCountry = (ip) => {
 
     return new Promise(async(resolve, reject) => {
         try {
             const res = await axios.get("https://www.cloudflare.com/cdn-cgi/trace");
             let arr = res.data.split("\n")
-            let ip = arr.filter(item => item.includes("ip"))[0].split("=")[1];
+            let ip2 = arr.filter(item => item.includes("ip"))[0].split("=")[1];
+            console.log(ip, ip2)
             
             if (!ip) reject("");
 
             const buffer = fs.readFileSync(`${process.cwd()}/public/GeoLite2-Country.mmdb`);
             const lookup = new Reader(buffer);
             const city = lookup.get(ip);
-
+            const city2 = lookup.get(ip2);
+            console.log(city.country, city2.country)
             if(city.country.names.en) resolve(city.country.names.en);
             else reject("");
         }
@@ -32,9 +34,10 @@ export const getCountry = () => {
 
 
 export const userDetails = async(data, req, res) => {
-    let country; 
+    let country;
     try {
-        country = await getCountry();
+        const ip = req.ip
+        country = await getCountry(ip);
     }
     catch (error) {
         country = "";
--- server/index.js
+++ server/index.js
@@ -12,6 +12,7 @@ const app = express();
 app.use(cors());
 app.use(fileupload());
 app.use(express.json());
+app.set('trust proxy', 'loopback')
 
 const uri = process.env.MONGODB_CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
@@ -24,6 +25,7 @@ app.use("/admin", adminRouter);
 
 app.get("/", (req, res) => {
     // main()
+    console.log(req.ip)
     return res.status(200).send("<h1>Hello World</h1>")
 })
 
@@ -37,7 +39,7 @@ try {
     })
     
     mongoose.connection.on('open', async () => {
-        app.listen(PORT, () => {
+        app.listen({port:PORT, host:'0.0.0.0'}, () => {
             console.log('Server started and database connected')
         })
     })
