32beb94d3d1cb6e7a9b003bf9a8f898f412e8fe4
anurag-AV-helix <anurag.av@helixstack.in>
2022-11-28 16:59:53 +0530

    FIle Size Issue Solved

--- server/index.js
+++ server/index.js
@@ -41,8 +41,8 @@ app.use(
 
 
 app.use(cors());
-app.use(fileupload());
-app.use(express.json());
+app.use(fileupload({limits:{fileSize:25*1024*1024}}));
+app.use(express.json({limit:"50mb"}));
 app.use(Sentry.Handlers.requestHandler());
 app.use(Sentry.Handlers.tracingHandler());
 app.set('trust proxy', 'loopback')
